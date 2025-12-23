8aeb674ead3a9c3f483f85eb5c4868039ac597d4
Per Eilef Koslung <per-eilef.koslung@telenor.no>
2025-12-23 09:04:28 +0100

    nxos source from git

--- aci-nxos-test/main.tf
+++ aci-nxos-test/main.tf
@@ -1,6 +1,8 @@
 module "nxos" {
-  source  = "netascode/nac-nxos/nxos"
-  version = ">= 0.1.0"
+  # source  = "netascode/nac-nxos/nxos"
+  # version = ">= 0.1.0"
+  # pull direct from source github
+  source = "git::https://github.com/netascode/terraform-nxos-nac-nxos.git?ref=main"
 
   yaml_files = ["system.nac.yaml"]
 }
